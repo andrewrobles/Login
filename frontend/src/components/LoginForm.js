129af097be442f1905231b1bf3d06b97a3da6465
andrewrobles <andrewroblesdev@gmail.com>
2021-10-10 20:11:13 -0700

    Fix floating input styling

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -24,24 +24,31 @@ class LoginForm extends React.Component {
       <main class="form-signin">
         <form onSubmit={e => this.props.handle_login(e, this.state)}>
           <h1 class="h3 mb-3 fw-normal d-flex justify-content-center">Please sign in</h1>
-          
-          <label htmlFor="username">Username</label>
-
-          <input
-            type="text"
-            name="username"
-            class="form-control"
-            value={this.state.username}
-            onChange={this.handle_change}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            class="form-control"
-            value={this.state.password}
-            onChange={this.handle_change}
-          />
+        
+          <div className="form-floating"> 
+            <input
+              type="text"
+              name="username"
+              className="form-control"
+              value={this.state.username}
+              onChange={this.handle_change}
+              id="floatingInput"
+              placeholder="Username"
+            />
+            <label for="floatingInput">Username</label>
+          </div>
+          <div className="form-floating">
+            <input
+              type="password"
+              name="password"
+              className="form-control"
+              value={this.state.password}
+              onChange={this.handle_change}
+              id="floatingPassword"
+              placeholder="Password"
+            />
+            <label for="floatingPassword">Password</label>
+          </div>
           <input class="w-100 btn btn-lg btn-primary mt-3" type="submit" />
         </form>
         <div class="mt-3">
